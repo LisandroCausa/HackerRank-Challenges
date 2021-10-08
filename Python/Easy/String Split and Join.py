890a6ee82aa7e94335461a8c9d5404d327905beb
Lisi <lisandro.anexa@gmail.com>
2021-10-08 19:43:51 -0300

    Create What's Your Name.py

--- Python/Easy/String Split and Join.py
+++ Python/Easy/String Split and Join.py
@@ -3,7 +3,7 @@
 # You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
 
 def split_and_join(line):
-    line.split(" ")
+    line = line.split(" ")
     line = "-".join(line)
     return line
 
