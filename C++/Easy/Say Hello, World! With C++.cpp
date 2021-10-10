37ec161581bf18b7d5b6fb8d4c42b125215e0414
Lisi <lisandro.anexa@gmail.com>
2021-10-10 20:57:07 -0300

    Create Say Hello, World! With C++.cpp

---
+++ C++/Easy/Say Hello, World! With C++.cpp
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+// Print "Hello, World!" to stdout.
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
