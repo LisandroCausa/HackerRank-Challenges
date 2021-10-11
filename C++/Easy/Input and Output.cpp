5fff7f9b4a67e629c2cc52c271abb0775d6246e6
Lisi <lisandro.anexa@gmail.com>
2021-10-10 21:05:56 -0300

    Create Input and Output.cpp

---
+++ C++/Easy/Input and Output.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+// Read 3 numbers from stdin and print their sum to stdout.
+
+#include <iostream>
+
+int main() {
+    int a;
+    int b;
+    int c;
+
+    std::cin >> a >> b >> c;
+
+    std::cout << a + b + c;
+
+    return 0;
+}
